cc813b934267d4b7d1e4c44da6942d7464c6e109
Pierre LESPINASSE <pilespin@e1r4p6.42.fr>
2017-12-19 14:05:35 +0100

    Refactore

---
+++ Element.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+# import re
+
+class Element(object):
+
+	def __init__(self, name="none"):
+		self._name = name
+		self._status = False
+		self._rules = []
+
+	def addRule(self, rule):
+		if (rule not in self._rules):
+			self._rules.append(rule)
+			# print "ADDED RULE: " + rule + " IN " + self._name
+
+	########## GETTER ##########
+	def getName(self):
+		return (self._name)
+
+	def getStatus(self):
+		return (self._status)
+
+	def getRules(self):
+		return (self._rules)
+	#############################
+
+	########## SETTER ##########
+	def setStatus(self, value=True):
+		self._status = value
+	#############################
+
+	def printInfo(self):
+		print "Name:   \"" + self._name + "\""
+		print "Status: " + str(self._status)
+		print "Rules: "
+		print self._rules
+		print ""
+
+	def printInfoInLine(self):
+		print 	"Name:   \"" 	+ self.getName() + "\"" + \
+				"	Status: " 	+ str(self.getStatus()) + \
+				"	Rules: " 	+ str(self.getRules())
--- expert.py
+++ expert.py
@@ -1,155 +1,97 @@
 #!/usr/bin/python
 
 import re
+from Element import *
 
-class Element(object):
+class Expert(object):
 
-	def __init__(self, name="none"):
-		self._name = name
-		self._status = False
-		self._rules = []
+	def __init__(self, data):
+		self.rules 		= []
+		self.facts 		= []
+		self.queries 	= []
+		self.elem 		= {}
 
-	def addRule(self, rule):
-		if (rule not in self._rules):
-			self._rules.append(rule)
-			# print "ADDED RULE: " + rule + " IN " + self._name
+		self.readFile(data)
+		self.initializeElement()
 
-	########## GETTER ##########
-	def getName(self):
-		return (self._name)
+		if (len(self.queries) <= 0):
+			print "Missing Queries"
+			exit(1)
 
-	def getStatus(self):
-		return (self._status)
+		self.facts = self.facts[0].split(" ")
+		self.queries = self.queries[0].split(" ")
 
-	def getRules(self):
-		return (self._rules)
-	#############################
+	def readFile(self, path):
+		with open(path) as f:
+			for line in f:
+				line = self.cleanLine(line)
+				if (len(line) > 0 and line[0] != "#"):
+					if (line[0] == "="):
+						self.facts.append(self.cleanLine(line[1:]));
+					elif (line[0] == "?"):
+						self.queries.append(self.cleanLine(line[1:]));
+					else:
+						self.rules.append(line);
 
-	########## SETTER ##########
-	def setStatus(self, value=True):
-		self._status = value
-	#############################
+	def initializeElement(self):
+		for s in self.rules:
+			iff = False
+			if (s.find("<=>") != -1):
+				ret = s.split("<=>")
+				iff = True
+			elif (s.find("=>") != -1):
+				ret = s.split("=>")
+			else:
+				print "ERROR: " + s + " Bad formulation"
+				exit(1)
 
-	def printInfo(self):
-		print "Name:   \"" + self._name + "\""
-		print "Status: " + str(self._status)
-		print "Rules: "
-		print self._rules
+			if (len(ret) != 2):
+				print "Error"
+				exit(1)
+			
+			rule = ret[0].strip()
+			implies = ret[1].strip()
+
+			if (len(rule) <= 0 or len(implies) <= 0):
+				print "Error: " + s
+				exit(1)
+
+			for n in self.splitLogicOperator(rule):
+				n = n.strip()
+				if (n not in self.elem):
+					self.elem[n] = Element(n)
+
+			for n in self.splitLogicOperator(implies):
+				n = n.strip()
+				if (n not in self.elem):
+					self.elem[n] = Element(n)
+				self.elem[n].addRule(rule)
+
+	def cleanLine(self, line):
+		line = line.strip()
+		end = line.find("#")
+		if (end == -1):
+			end = len(line)
+		line = line[0:end]
+		line = " ".join(line.split())
+		line = line.strip()
+		return (line)
+
+	def splitLogicOperator(self, string):
+		return (re.split('\+|\||\^', string))
+
+	def printAll(self):
+		print "RULES:"
+		for elt in self.rules:
+			print(elt)
 		print ""
 
-	def printInfoInLine(self):
-		print 	"Name:   \"" 	+ self.getName() + "\"" + \
-				"	Status: " 	+ str(self.getStatus()) + \
-				"	Rules: " 	+ str(self.getRules())
+		print "FACT:"
+		for elt in self.facts:
+			print(elt)
+		print ""
 
-###############################################################################
-
-def printAll():
-	print "RULES:"
-	for elt in rules:
-		print(elt)
-	print ""
-
-	print "FACT:"
-	for elt in facts:
-		print(elt)
-	print ""
-
-	print "QUERIES:"
-	for elt in queries:
-		print(elt)
-	print ""
-
-def cleanLine(line):
-	line = line.strip()
-	end = line.find("#")
-	if (end == -1):
-		end = len(line)
-	line = line[0:end]
-	line = " ".join(line.split())
-	line = line.strip()
-	return (line)
-
-def readFile(path):
-	rules 	= []
-	facts 	= []
-	queries	= []
-
-	with open(path) as f:
-		for line in f:
-			line = cleanLine(line)
-			if (len(line) > 0 and line[0] != "#"):
-				if (line[0] == "="):
-					facts.append(cleanLine(line[1:]));
-				elif (line[0] == "?"):
-					queries.append(cleanLine(line[1:]));
-				else:
-					rules.append(line);
-	facts = facts[0].split(" ")
-	queries = queries[0].split(" ")
-	return(rules, facts, queries)
-
-def splitLogicOperator(string):
-	return (re.split('\+|\||\^', string))
-
-def initializeElement():
-	elem = {}
-
-	for s in rules:
-		iff = False
-		if (s.find("<=>") != -1):
-			ret = s.split("<=>")
-			iff = True
-		elif (s.find("=>") != -1):
-			ret = s.split("=>")
-		else:
-			print "ERROR: " + s + " Bad formulation"
-			exit(1)
-
-		if (len(ret) != 2):
-			print "Error"
-			exit(1)
-		
-		rule = ret[0].strip()
-		implies = ret[1].strip()
-
-		if (len(rule) <= 0 or len(implies) <= 0):
-			print "Error: " + s
-			exit(1)
-
-		for n in splitLogicOperator(rule):
-			n = n.strip()
-			if (n not in elem):
-				elem[n] = Element(n)
-
-		for n in splitLogicOperator(implies):
-			n = n.strip()
-			if (n not in elem):
-				elem[n] = Element(n)
-			elem[n].addRule(rule)
-	return (elem)
-
-##############################
-############ MAIN ############
-##############################
-
-rules, facts, queries = readFile('data')
-elem = initializeElement()
-
-if (len(queries) <= 0):
-	print "Missing Queries"
-	exit(1)
-
-printAll()
-
-print facts
-
-for i in facts:
-	elem[i].setStatus()
-
-for i in elem:
-	print elem[i].printInfoInLine()
-
-for i in elem:
-	if elem[i].getName()[0] != "!" and i in queries:
-		print elem[i].getName() + " = " + str(elem[i].getStatus())
+		print "QUERIES:"
+		for elt in self.queries:
+			print(elt)
+		print ""	
